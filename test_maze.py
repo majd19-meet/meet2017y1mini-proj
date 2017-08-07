91f3a9809ea29d2b711a41352c73db8f90aca055
majd19-meet <majd19@meet.mit.edu>
2017-08-07 16:55:57 +0300

    Stopping here

---
+++ test_maze.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+SIZE_X=1000
+SIZE_Y=800
+turtle.setup(SIZE_X, SIZE_Y)
+
+turtle.penup()
+
+SQUARE_SIZE=20
+
+pos_list=[]
+stamp_list=[]
+
+fat_man = turtle.clone()
+fat_man.shape('trash.gif')
+
